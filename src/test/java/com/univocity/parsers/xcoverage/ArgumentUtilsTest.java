d5e42824ecd0d7cd588c5632f54b4292b556557e
Simon Edström Kawaji <kawaji@kth.se>
2020-02-18 15:58:16 +0100

    [fix] Add branch coverage ratio to indexOf() and trim()
    Resolves #39

--- src/test/java/com/univocity/parsers/xcoverage/ArgumentUtilsTest.java
+++ src/test/java/com/univocity/parsers/xcoverage/ArgumentUtilsTest.java
@@ -1,5 +1,6 @@
 package com.univocity.parsers.xcoverage;
 
+import com.univocity.parsers.annotations.helpers.AnnotationHelper;
 import com.univocity.parsers.common.ArgumentUtils;
 import com.univocity.parsers.fixed.FixedWidthFields;
 import com.univocity.parsers.fixed.FixedWidthWriter;
@@ -31,12 +32,25 @@ public class ArgumentUtilsTest {
         for(int i = 0; i < ArgumentUtils.trimBranchCoverage.length; i++){
             System.out.println("Trim branch " + i + " = " +ArgumentUtils.trimBranchCoverage[i]);
         }
+        System.out.println("Coverage: " + getRatio(ArgumentUtils.trimBranchCoverage));
         System.out.println("======================================\n");
 
         System.out.println("=== DIY branch coverage for indexOf() ===");
         for(int i = 0; i < ArgumentUtils.indexOfBranchCoverage.length; i++){
             System.out.println("Trim branch " + i + " = " +ArgumentUtils.indexOfBranchCoverage[i]);
         }
+        System.out.println("Coverage: " + getRatio(ArgumentUtils.indexOfBranchCoverage));
+
         System.out.println("======================================");
+    }
+
+    private double getRatio(boolean[] branchCoverage){
+        int tot = 0;
+        for (boolean b : branchCoverage){
+            if (b){
+                tot += 1;
+            }
+        }
+        return (double) tot / (double) branchCoverage.length;
     }
 }
