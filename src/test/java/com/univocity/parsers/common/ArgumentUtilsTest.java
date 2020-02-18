445f77b2d9633010acd894e6e9835d2bc5b13ae3
Simon Edström Kawaji <kawaji@kth.se>
2020-02-18 16:21:46 +0100

    [feat] Add an additional test for indexOf() function
    Resolves #40

--- src/test/java/com/univocity/parsers/common/ArgumentUtilsTest.java
+++ src/test/java/com/univocity/parsers/common/ArgumentUtilsTest.java
@@ -6,12 +6,36 @@ import static org.testng.Assert.*;
 
 public class ArgumentUtilsTest {
 
+    public class testClass{
+        int x;
+    }
+
     /**
-     * Cover the first branch of the function where the input array is null ot increase
-     * branch coverage. This will throw an NullPointerException "Null Array".
+     * Cover the 5th branch where the input element and the input array don't have the same
+     * class type. This should throw an IllegalStateException with message "a". This did not
+     * have any coverage before this test.
      */
     @Test
-    public void testIndexOf() {
+    public void testIndexOf1() {
+        testClass[] testArray = new testClass[5];
+        String testObject = "hello";
+        int testFrom = 0;
+
+        try {
+            ArgumentUtils.publicIndexOf(testArray, testObject, testFrom);
+            fail();
+        } catch (IllegalStateException e) {
+            assertEquals(e.getMessage(), "a");
+        }
+    }
+
+    /**
+     * Cover the first branch of the function where the input array is null ot increase
+     * branch coverage. This will throw an NullPointerException "Null Array". Did not have
+     * coverage before this test.
+     */
+    @Test
+    public void testIndexOf2() {
         Object[] testArray = null;
         Object testObject = new Object();
         int testFrom = 0;
